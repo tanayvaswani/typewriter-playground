2d47b9e9445a7451f8e6550d811b056fc235aadd
tanayvaswani <vaswani.tanay9@gmail.com>
2024-03-08 00:38:59 +0530

    feat: playground class

--- apps/server/classes/playground.ts
+++ apps/server/classes/playground.ts
@@ -1,1 +1,20 @@
-export class Playground {}
+import { Server } from "socket.io";
+
+export class Playground {
+  playgroundStatus: "not-started" | "in-progress" | "finished";
+  gameId: string;
+  players: { id: string; score: number; name: string }[];
+  io: Server;
+  playgroundHost: string;
+  paragraph: string;
+
+  constructor(id: string, io: Server, host: string) {
+    this.gameId = id;
+    this.io = io;
+    this.playgroundHost = host;
+
+    this.players = [];
+    this.playgroundStatus = "not-started";
+    this.paragraph = "";
+  }
+}
