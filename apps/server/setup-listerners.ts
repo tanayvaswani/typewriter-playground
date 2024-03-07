9a481cae8cf8fbaf736ec302e140b7033b2c7ad1
tanayvaswani <vaswani.tanay9@gmail.com>
2024-03-08 00:33:28 +0530

    feat: join-playground

--- apps/server/setup-listerners.ts
+++ apps/server/setup-listerners.ts
@@ -12,15 +12,19 @@ export function setupListerner(io: Server) {
     console.log(`New Connection - ${socket.id}`);
 
     socket.on("join-playground", (roomId: string, name: string) => {
-      if (!roomId) {
-        return socket.emit("error", "Invalid Room ID.");
-      }
-
-      if (!name) {
-        return socket.emit("error", "Please enter a valid name.");
-      }
+      if (!roomId) return socket.emit("error", "Invalid Room ID.");
+      if (!name) return socket.emit("error", "Please enter a valid name.");
 
       socket.join(roomId);
+
+      if (playgrounds.has(roomId)) {
+        const playground = playgrounds.get(roomId);
+
+        if (!playground) {
+          return socket.emit("error", "Playground doesn't exists");
+        }
+      } else {
+      }
     });
   });
 }
